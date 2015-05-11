305c70bbd5a8f06dfac0c8b6097e17cd529584a5
A.J. Rouvoet <a.j.rouvoet@gmail.com>
2015-05-11 12:44:18 +0200

    Added form for posting on a draad

--- src/forum/views.py
+++ src/forum/views.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.shortcuts import render
+from django import forms
 from base.views import render_with_layout
 from base.utils import grouped_dict
 from .models import *
 
+from datetime import datetime
 import logging
 logger = logging.getLogger(__name__)
 
@@ -26,6 +28,32 @@ class ForumMostRecentTable(ForumTable):
     model = ForumDraad
     fields = ('uid_id', 'titel', 'laatst_gewijzigd', 'laatste_wijziging_uid_id')
     order_by = ('-laatst_gewijzigd',)
+
+class ForumPostForm(forms.ModelForm):
+  class Meta:
+    model = ForumPost
+    fields = ['draad', 'tekst']
+    widgets = {
+      'draad': forms.HiddenInput()
+    }
+
+  def __init__(self, *args, **kwargs):
+    self.user = kwargs.pop('user', None)
+    super().__init__(*args, **kwargs)
+
+  def save(self, commit=True):
+    post = super().save(commit=False)
+
+    post.user = Profiel.objects.get(user=self.user)
+    post.datum_tijd = datetime.now() #TODO
+    post.laatst_gewijzigd = datetime.now()
+    post.verwijderd = False
+    post.auteur_ip = "" # TODO
+    post.wacht_goedkeuring = -1 #TODO
+
+    if commit: post.save()
+
+    return post
 
 def forum_root(request):
   # TODO filter viewable by user
@@ -51,16 +79,32 @@ def forum_root(request):
 
 def forum_draad(request, draad_id):
   draad = ForumDraad.objects.get(pk=draad_id)
+  fresh_form = ForumPostForm(initial={'draad': draad})
+
+  # new post form
+  if request.method == 'POST':
+    form = ForumPostForm(request.POST, user=request.user)
+
+    if form.is_valid():
+      form.save()
+
+      # use a fresh form for the next page
+      form = fresh_form
+
+  else:
+    form = fresh_form
+
   posts = ForumPost.objects.all()\
     .filter(draad_id=draad_id)\
     .order_by('datum_tijd')
 
   return render_with_layout(request, 'forum_draad.jade', title="Forum draad %s" % draad.titel, ctx={
     'draad': draad,
-    'posts': posts
+    'posts': posts,
+    'new_post_form': form
   })
 
 urls = patterns('',
    url(r'^$', forum_root, name='forum.main'),
-   url(r'^(\d+)/', forum_draad, name='forum.draad'),
+   url(r'^(\d+)/$', forum_draad, name='forum.draad')
 )
