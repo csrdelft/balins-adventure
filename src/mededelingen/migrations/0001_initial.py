3b4a65df95621c7055dd4a34b0c0f89cd560601d
rvanheukelum <R.H.vanHeukelum@student.tudelft.nl>
2015-07-07 21:12:38 +0200

    Changed dependency

--- src/mededelingen/migrations/0001_initial.py
+++ src/mededelingen/migrations/0001_initial.py
@@ -7,25 +7,26 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0011_auto_20150707_1845'),
+        ('base', '0010_optional_fields_fixes'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Mededeling',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('datum', models.DateTimeField(null=True, blank=True)),
                 ('vervaltijd', models.DateTimeField(null=True, blank=True)),
                 ('titel', models.TextField()),
                 ('tekst', models.TextField()),
                 ('prive', models.CharField(max_length=1)),
+                ('zichtbaarheid', models.CharField(max_length=17)),
                 ('prioriteit', models.IntegerField()),
                 ('doelgroep', models.CharField(max_length=10)),
                 ('verborgen', models.CharField(max_length=1)),
                 ('verwijderd', models.CharField(max_length=1)),
                 ('plaatje', models.CharField(max_length=255)),
-                ('user', models.ForeignKey(max_length=4, db_column='uid', to='base.Profiel')),
+                ('user', models.ForeignKey(to='base.Profiel', db_column='uid', max_length=4)),
             ],
             options={
                 'db_table': 'mededeling',
