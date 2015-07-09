6585606b2ea15ac2e7472087b0cd0aedc5b427b0
rvanheukelum <R.H.vanHeukelum@student.tudelft.nl>
2015-07-09 21:54:14 +0200

    Attempt to trigger build succes

--- src/mededelingen/tests.py
+++ src/mededelingen/tests.py
@@ -1,11 +1,21 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory, APITestCase, force_authenticate
+from base.models import Profiel
 from mededelingen.models import Mededeling
 from autofixture import AutoFixture
-class MededelingTest(TestCase):
+
+
+class MededelingTest(APITestCase, TestCase):
 
   def setUp(self):
     AutoFixture(Mededeling, generate_fk=True, field_values={
-      'titel': 'Nice day today'
+      'titel': 'Nice day today',
+      'audience': 'LID'
+    }).create(1)
+
+    AutoFixture(Profiel, generate_fk=True, field_values={
+      'uid': '0001',
+      'status': 'OUD'
     }).create(1)
 
   def test_titel(self):
@@ -13,4 +23,12 @@ class MededelingTest(TestCase):
     self.assertEqual(alpha.pk, 1)
     self.assertEqual(alpha.titel, 'Nice day today')
 
-
+  # def test_post_bestuur(self):
+  #   factory = APIRequestFactory()
+  #   user = Profiel.objects.get(uid='0001')
+  #   view = Mededeling.as_view()
+  #
+  #   request = factory.get('/api/v1/mededelingen/1')
+  #   force_authenticate(request,user=user)
+  #   response = view(request)
+  #   print(response)
