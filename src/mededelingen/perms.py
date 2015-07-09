c4de397467f630c4bdd2ceb902f5eef62bce6120
A.J. Rouvoet <a.j.rouvoet@gmail.com>
2015-07-09 16:25:31 +0200

    Formatting fix

--- src/mededelingen/perms.py
+++ src/mededelingen/perms.py
@@ -2,11 +2,13 @@ from base.perms import *
 from mededelingen.models import Mededeling
 from base.models import Bestuur
 
-##########    WIP
-
 def get_ht_bestuur(): return Bestuur.objects.get(status='ht')
 
-PERMISSION_LOGICS = [(
-  (Mededeling, InGroupPermissionLogic(['mededelingen.add_mededeling','mededelingen.delete_mededeling',
-                                       'mededelingen.change_mededeling'],get_ht_bestuur))
-)]
+PERMISSION_LOGICS = [
+  (Mededeling, InGroupPermissionLogic(
+    [ 'mededelingen.add_mededeling',
+      'mededelingen.delete_mededeling',
+      'mededelingen.change_mededeling' ]
+    , get_ht_bestuur)
+  )
+]
