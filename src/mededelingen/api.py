ee202cffadc57a332243e8e8c684472b8eadc771
rvanheukelum <R.H.vanHeukelum@student.tudelft.nl>
2015-07-09 14:49:58 +0200

    WIP on create/delete perms

--- src/mededelingen/api.py
+++ src/mededelingen/api.py
@@ -1,11 +1,24 @@
 from rest_framework import mixins, viewsets
 from mededelingen.serializers import MededelingenSerializer
 from mededelingen.models import Mededeling
+from base.utils import deny_on_fail
 
 class MededelingenViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, mixins.CreateModelMixin,
-  mixins.DestroyModelMixin, viewsets.GenericViewSet):
+  mixins.DestroyModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet):
 
   serializer_class = MededelingenSerializer
 
   def get_queryset(self):
     return Mededeling.get_viewable_by(self.request.user)
+
+  def destroy(self, request, *args, **kwargs):
+    deny_on_fail(request.user.has_perm('mededelingen.destroy', self.get_object()))
+    super(request, *args, **kwargs)
+
+  def create(self, request, *args, **kwargs):
+    print('-----------get object--------------')
+    instance = self.get_object()
+    print('-----------entered-----------------')
+    deny_on_fail(request.user.has_perm('mededelingen.create', instance))
+    print('-----------not failed--------------')
+    super()
--- src/mededelingen/perms.py
+++ src/mededelingen/perms.py
@@ -6,19 +6,6 @@ from base.models import Bestuur
 
 def get_ht_bestuur(): return Bestuur.objects.get(status='ht')
 
-PERMISSION_LOGICS = (
-  (Mededeling, DynamicConditionLogic(['mededeling.add_mededeling'], 'rechten_toevoegen')),
-  (Mededeling, DynamicConditionLogic(['mededeling.delete_mededeling'], 'rechten_verwijderen')),
-  (Mededeling, DynamicConditionLogic(['mededeling.view_mededeling'], 'rechten_lezen')),
-  (Mededeling, DynamicConditionLogic(['mededeling.change_mededeling'], 'rechten_veranderen')),
-  (Mededeling, InGroupPermissionLogic(['mededelingen.add_mededeling'], get_ht_bestuur)),
-  (Mededeling, InGroupPermissionLogic(['mededelingen.change_mededeling'], get_ht_bestuur())),
-  (Mededeling, InGroupPermissionLogic(['mededelingen.delete_mededeling'], get_ht_bestuur())),
-  (Mededeling, MatchFieldPermissionLogic(
-          grants=['mededelingen.view'],
-          user_attr='profiel__lidstatus',
-          obj_attr='zichtbaarheid',
-        )),
-)
-
-
+PERMISSION_LOGICS = [(
+  (Mededeling, InGroupPermissionLogic(['mededelingen.create', 'mededelingen.destroy'],get_ht_bestuur))
+)]
