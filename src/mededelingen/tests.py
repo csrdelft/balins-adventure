88ead66f931c3c8a1eee2ad9238a96b07324f8ed
A.J. Rouvoet <a.j.rouvoet@gmail.com>
2015-07-10 14:42:33 +0200

    Fixed mededelingen tests Autofixture couldn't deal with user_id (but can deal with 'user' field value)

--- src/mededelingen/tests.py
+++ src/mededelingen/tests.py
@@ -13,95 +13,97 @@ class MededelingTests(APITestCase):
     ouduser = User.objects.create_user(username='OudLid', password='OudLid')
 
     AutoFixture(Profiel, field_values={
-      'naam': 'LID',
+      'voornaam': 'LID',
       'status': Profiel.STATUS.LID,
-      'user_id': liduser.pk
+      'user': liduser,
     }).create(1)
 
     AutoFixture(Profiel, field_values={
-      'naam': 'OUDLID',
+      'voornaam': 'OUDLID',
       'status': Profiel.STATUS.OUDLID,
-      'user_id': ouduser.pk
+      'user': ouduser
     }).create(1)
+    print(Profiel.objects.all().values('pk', 'user_id', 'status'))
 
     AutoFixture(Mededeling, generate_fk=True, field_values={
       'pk': 1,
       'titel': 'A',
-      'audience': Mededeling.AUDIENCE.PUBLIC
+      'audience': Mededeling.AUDIENCE.PUBLIC,
+      'live': True
     }).create(1)
 
     AutoFixture(Mededeling, generate_fk=True, field_values={
       'pk': 2,
       'titel': 'B',
-      'audience': Mededeling.AUDIENCE.LEDEN
+      'audience': Mededeling.AUDIENCE.LEDEN,
+      'live': True
     }).create(1)
 
     AutoFixture(Mededeling, generate_fk=True, field_values={
       'pk': 3,
       'titel': 'C',
-      'audience': Mededeling.AUDIENCE.OUDLEDEN
+      'audience': Mededeling.AUDIENCE.OUDLEDEN,
+      'live': True
     }).create(1)
 
-  def fixture_lid(self):
-    self.fixture()
-    self.client.login(username="Lid", password="Lid")
-
-  def fixture_oudlid(self):
-    self.fixture()
-    self.client.login(username="OudLid", password="OudLid")
-
+    print(Mededeling.objects.all().values('pk', 'live', 'audience'))
 
   def test_public_gets_public(self):
     self.fixture()
-    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 1}), {})
+    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 1}))
     self.assertEqual(response.status_code, status.HTTP_200_OK)
     self.assertEqual(response.data['titel'], 'A')
 
   def test_public_gets_lid(self):
     self.fixture()
-    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 2}), {})
+    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 2}))
     self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
   def test_public_gets_oudlid(self):
     self.fixture()
-    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 3}), {})
+    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 3}))
     self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 
   def test_lid_gets_public(self):
-    self.fixture_lid()
-    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 1}), {})
+    self.fixture()
+    self.client.login(username="Lid", password="Lid")
+    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 1}))
     self.assertEqual(response.status_code, status.HTTP_200_OK)
     self.assertEqual(response.data['titel'], 'A')
 
   def test_lid_gets_lid(self):
-    self.fixture_lid()
-    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 2}), {})
+    self.fixture()
+    self.client.login(username="Lid", password="Lid")
+    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 2}))
     self.assertEqual(response.status_code, status.HTTP_200_OK)
     self.assertEqual(response.data['titel'], 'B')
 
   def test_lid_gets_oudlid(self):
-    self.fixture_lid()
-    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 3}), {})
+    self.fixture()
+    self.client.login(username="Lid", password="Lid")
+    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 3}))
     self.assertEqual(response.status_code, status.HTTP_200_OK)
     self.assertEqual(response.data['titel'], 'C')
 
 
   def test_oudlid_gets_public(self):
-    self.fixture_oudlid()
-    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 1}), {})
+    self.fixture()
+    self.client.login(username="OudLid", password="OudLid")
+    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 1}))
     self.assertEqual(response.status_code, status.HTTP_200_OK)
     self.assertEqual(response.data['titel'], 'A')
 
   def test_oudlid_gets_lid(self):
-    self.fixture_oudlid()
-    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 2}), {})
+    self.fixture()
+    self.client.login(username="OudLid", password="OudLid")
+    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 2}))
     self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
   def test_oudlid_gets_oudlid(self):
-    self.fixture_oudlid()
-    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 3}), {})
+    self.fixture()
+    self.client.login(username="OudLid", password="OudLid")
+    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 3}))
     self.assertEqual(response.status_code, status.HTTP_200_OK)
     self.assertEqual(response.data['titel'], 'C')
-
 
