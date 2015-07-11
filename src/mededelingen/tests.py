79a0b6c12e44518ed03a42d910d4521557de194e
rvanheukelum <R.H.vanHeukelum@student.tudelft.nl>
2015-07-12 00:08:03 +0200

    Added post tests

--- src/mededelingen/tests.py
+++ src/mededelingen/tests.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, resolve
 from rest_framework import status
 from rest_framework.test import APITestCase
 from base.models import Profiel, Bestuur, BestuursLid
@@ -17,26 +17,29 @@ class MededelingTests(APITestCase):
     AutoFixture(Profiel, field_values={
       'voornaam': 'LID',
       'status': Profiel.STATUS.LID,
-      'user': liduser
+      'user': liduser,
+      'uid': '0001'
     }).create(1)
 
     AutoFixture(Profiel, field_values={
       'voornaam': 'OUDLID',
       'status': Profiel.STATUS.OUDLID,
-      'user': ouduser
+      'user': ouduser,
+      'uid': '0002'
     }).create(1)
 
     AutoFixture(Profiel, field_values={
       'voornaam': 'SUPER',
       'status': Profiel.STATUS.LID,
-      'user': superuser
+      'user': superuser,
+      'uid': '0003'
     }).create(1)
 
     AutoFixture(Profiel, field_values={
       'voornaam': 'BESTUURSLID',
       'status': Profiel.STATUS.LID,
       'user': bestuuruser,
-      'uid': '1234'
+      'uid': '0004'
     }).create(1)
 
     AutoFixture(Bestuur, generate_fk=True, field_values={
@@ -45,7 +48,7 @@ class MededelingTests(APITestCase):
     }).create(1)
 
     AutoFixture(BestuursLid, field_values={
-      'user': Profiel.objects.get(uid='1234'),
+      'user': Profiel.objects.get(uid='0004'),
       'opmerking': 'Praeses van Heukelum',
       'groep_id': Bestuur.objects.get(status='ht')
     }).create(1)
@@ -71,6 +74,13 @@ class MededelingTests(APITestCase):
       'live': True
     }).create(1)
 
+    self.data = {
+      'titel': 'E',
+      'tekst': 'Newly posted Announcement',
+      'prioriteit': '0',
+      'user': '0004',
+      'plaatje': 'NULL',
+    }
 
   def test_public_gets_public(self):
     self.fixture()
@@ -235,7 +245,7 @@ class MededelingTests(APITestCase):
     self.assertEqual(status.HTTP_200_OK, response_3.status_code)
     self.assertEqual('A', response_3.data['titel'])
 
-  def test_public_patch(self):
+  def test_public_delete(self):
     self.fixture()
 
     response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 1}))
@@ -248,3 +258,57 @@ class MededelingTests(APITestCase):
     response_3 = self.client.get(reverse('mededeling-detail', kwargs={'pk': 1}))
     self.assertEqual(status.HTTP_200_OK, response_3.status_code)
     self.assertEqual('A', response_3.data['titel'])
+
+
+  def test_superuser_post(self):
+    self.fixture()
+    self.client.login(username='Superman', password='Superman')
+
+    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 4}))
+    self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
+
+    response_2 = self.client.post(reverse('mededeling-list'), self.data, format='json')
+    self.assertEqual(status.HTTP_201_CREATED, response_2.status_code)
+
+    response_3 = self.client.get(reverse('mededeling-detail', kwargs={'pk': 4}))
+    self.assertEqual(status.HTTP_200_OK, response_3.status_code)
+    self.assertEqual('E', response_3.data['titel'])
+
+  def test_bestuur_post(self):
+    self.fixture()
+    self.client.login(username='Bestuur', password='Bestuur')
+
+    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 4}))
+    self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
+
+    response_2 = self.client.post(reverse('mededeling-list'), self.data, format='json')
+    self.assertEqual(status.HTTP_201_CREATED, response_2.status_code)
+
+    response_3 = self.client.get(reverse('mededeling-detail', kwargs={'pk': 4}))
+    self.assertEqual(status.HTTP_200_OK, response_3.status_code)
+    self.assertEqual('E', response_3.data['titel'])
+
+  def test_lid_post(self):
+    self.fixture()
+    self.client.login(username='Lid', password='Lid')
+
+    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 4}))
+    self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
+
+    response_2 = self.client.post(reverse('mededeling-list'), self.data, format='json')
+    self.assertEqual(status.HTTP_403_FORBIDDEN, response_2.status_code)
+
+    response_3 = self.client.get(reverse('mededeling-detail', kwargs={'pk': 4}))
+    self.assertEqual(status.HTTP_404_NOT_FOUND, response_3.status_code)
+
+  def test_public_post(self):
+    self.fixture()
+
+    response = self.client.get(reverse('mededeling-detail', kwargs={'pk': 4}))
+    self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
+
+    response_2 = self.client.post(reverse('mededeling-list'), self.data, format='json')
+    self.assertEqual(status.HTTP_403_FORBIDDEN, response_2.status_code)
+
+    response_3 = self.client.get(reverse('mededeling-detail', kwargs={'pk': 4}))
+    self.assertEqual(status.HTTP_404_NOT_FOUND, response_3.status_code)
