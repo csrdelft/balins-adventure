ecd0dce9ea490f192650aef3e7663e958cb433a6
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2016-04-03 17:21:52 +0200

    Inline group members in Base admin

--- src/base/admin.py
+++ src/base/admin.py
@@ -1,23 +1,85 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Profiel)
-admin.site.register(Groep)
-admin.site.register(Ketzer)
-admin.site.register(Kring)
-admin.site.register(Werkgroep)
-admin.site.register(Lichting)
-admin.site.register(Ondervereniging)
-admin.site.register(Verticale)
-admin.site.register(Activiteit)
-admin.site.register(Bestuur)
+class GroepsLidInline(admin.TabularInline):
+    model = GroepsLid
 
-admin.site.register(GroepsLid)
-admin.site.register(KetzerDeelnemer)
-admin.site.register(KringLid)
-admin.site.register(WerkgroepDeelnemer)
-admin.site.register(LichtingLid)
-admin.site.register(OnderverenigingsLid)
-admin.site.register(VerticaleLid)
-admin.site.register(ActiviteitDeelnemer)
-admin.site.register(BestuursLid)
+class GroepAdmin(admin.ModelAdmin):
+    inlines = [
+        GroepsLidInline,
+    ]
+
+class KetzerDeelnemerInline(admin.TabularInline):
+    model = KetzerDeelnemer
+
+class KetzerAdmin(admin.ModelAdmin):
+    inlines = [
+        KetzerDeelnemerInline,
+    ]
+
+class KringLidInline(admin.TabularInline):
+    model = KringLid
+
+class KringAdmin(admin.ModelAdmin):
+    inlines = [
+        KringLidInline,
+    ]
+
+class WerkgroepDeelnemerInline(admin.TabularInline):
+    model = WerkgroepDeelnemer
+
+class WerkgroepAdmin(admin.ModelAdmin):
+    inlines = [
+        WerkgroepDeelnemerInline,
+    ]
+
+class LichtingLidInline(admin.TabularInline):
+    model = LichtingLid
+
+class LichtingAdmin(admin.ModelAdmin):
+    inlines = [
+        LichtingLidInline,
+    ]
+
+class OnderverenigingsLidInline(admin.TabularInline):
+    model = OnderverenigingsLid
+
+class OnderverenigingAdmin(admin.ModelAdmin):
+    inlines = [
+        OnderverenigingsLidInline,
+    ]
+
+class VerticaleLidInline(admin.TabularInline):
+    model = VerticaleLid
+
+class VerticaleAdmin(admin.ModelAdmin):
+    inlines = [
+        VerticaleLidInline,
+    ]
+
+class ActiviteitDeelnemerInline(admin.TabularInline):
+    model = ActiviteitDeelnemer
+
+class ActiviteitAdmin(admin.ModelAdmin):
+    inlines = [
+        ActiviteitDeelnemerInline,
+    ]
+
+class BestuursLidInline(admin.TabularInline):
+    model = BestuursLid
+
+class BestuurAdmin(admin.ModelAdmin):
+    inlines = [
+        BestuursLidInline,
+    ]
+
+admin.site.register(Profiel)
+admin.site.register(Groep, GroepAdmin)
+admin.site.register(Ketzer, KetzerAdmin)
+admin.site.register(Kring, KringAdmin)
+admin.site.register(Werkgroep, WerkgroepAdmin)
+admin.site.register(Lichting, LichtingAdmin)
+admin.site.register(Ondervereniging, OnderverenigingAdmin)
+admin.site.register(Verticale, VerticaleAdmin)
+admin.site.register(Activiteit, ActiviteitAdmin)
+admin.site.register(Bestuur, BestuurAdmin)
